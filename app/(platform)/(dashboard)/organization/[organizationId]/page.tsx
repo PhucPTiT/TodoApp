94d814e31cc76b935574d0cc176b75b7ef35a1da
PhucPTiT <phucndah2002@gmail.com>
2024-01-10 15:00:58 +0700

    push code

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,6 +1,20 @@
-const OrganizationIdPage = () => {
+import { db } from "@/lib/db";
+import { Board } from "./board";
+import { Form } from "./form";
+
+const OrganizationIdPage = async() => {
+    const boards = await db.board.findMany();
     return (
-        <div>Test</div>
+        <div className="" >
+            <Form/>
+            <div className="space-y-2">
+                {
+                    boards.map((board) => (
+                        <Board key = {board.id} id = {board.id} title={board.title}/>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
 
