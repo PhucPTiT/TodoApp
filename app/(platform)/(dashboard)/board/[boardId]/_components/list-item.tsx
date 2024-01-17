99768892a1c66252c2c3a45e8c32fb0795169a0d
PhucPTiT <phucndah2002@gmail.com>
2024-01-17 16:53:58 +0700

    add fuctions

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { ListWithCards } from "@/type"
+import { ListHeader } from "./list-header";
+import { ElementRef, useRef, useState } from "react";
+import { CardForm } from "./card-form";
+
+interface ListItemProps {
+    data: ListWithCards;
+    index: number;
+}
+export const ListItem = ({data, index} : ListItemProps) => {
+    const textareaRef = useRef<ElementRef<"textarea">>(null);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const disableEditing = () => {
+        setIsEditing(false);
+    };
+
+    const enableEditing = () => {
+        setIsEditing(true);
+        setTimeout(() => {
+            textareaRef.current?.focus();
+        })
+    }
+
+    return (
+        <li className="round-md shrink-0 h-full w-[272px] select-none">
+            <div className="w-full bg-[#f1f2f4] shadow-md pb-2 rounded-md">
+                <ListHeader data = {data} onAddCard = {enableEditing}/>
+                <CardForm
+                    listId = {data.id}
+                    isEditing = {isEditing}
+                    enabledEditing = {enableEditing}
+                    disabledEditing = {disableEditing}
+                    ref = {textareaRef}
+                />
+            </div>
+        </li>
+    )
+}
