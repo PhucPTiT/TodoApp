21d0fd0c877ad65c43ab671687bae07a621a9964
PhucPTiT <phucndah2002@gmail.com>
2024-01-22 17:47:38 +0700

    adđ function with cart item

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,3 +1,5 @@
+import { ModalProvider } from "@/components/providers/modal-provider"
+import { QueryProvider } from "@/components/providers/query-provider"
 import { ClerkProvider } from "@clerk/nextjs"
 import React from "react"
 import {Toaster} from "sonner"
@@ -7,8 +9,11 @@ const PlatformLayout = (
 ) => {
     return (
         <ClerkProvider>
-            <Toaster/>
-            {children}
+            <QueryProvider>
+                <Toaster/>
+                <ModalProvider/>
+                {children}
+            </QueryProvider>
         </ClerkProvider>
     )
 } 
